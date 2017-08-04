56ebe2c3e424e09f7b7969676fc3c48c195dbc5d
rena2henriques <renato.manao@gmail.com>
2017-08-04 12:31:56 +0100

    initiating project

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+// setting up express
+const express = require('express');
+var app = express();
+
+// path to public folder
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+app.listen(3000, () => {
+	console.log(`Listening on port ${port}.`)
+});
