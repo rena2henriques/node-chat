f0c3506df2987b691d6350136632f2ea52be5745
rena2henriques <renato.manao@gmail.com>
2017-08-04 17:35:33 +0100

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -22,6 +22,20 @@ io.on('connection', (socket) => {
 	// prints when a new user connect
 	console.log('New user connected');
 
+	// send a message to the user when it enters the chat app
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app',	
+		createdAt: new Date().getTime()
+	});
+
+	// sends a message to every other user but himself
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 
@@ -30,6 +44,12 @@ io.on('connection', (socket) => {
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+
+		/*socket.broadcast.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});*/
 	})
 
 	// detects when a user disconnects
