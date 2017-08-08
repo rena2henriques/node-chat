e42cfb8969902db81bb1717c5fdc5a4482ad0613
rena2henriques <renato.manao@gmail.com>
2017-08-08 12:02:22 +0100

    Added timestamps to messages

--- public/js/index.js
+++ public/js/index.js
@@ -15,19 +15,23 @@ socket.on('disconnect', function() {
 
 // prints new message when the browser senses a new email
 socket.on('newMessage', function(message) {
-	console.log('New Message', message);
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	if (message.text != ''){
+		var li = jQuery('<li></li>');
+		li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-	var li = jQuery('<li></li>');
-	li.text(`${message.from}: ${message.text}`);
-
-	jQuery('#messages').append(li);
+		jQuery('#messages').append(li);
+	}
+	
 });
 
 socket.on('newLocationMessage', function(message) {
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+
 	var li = jQuery('<li></li>');
 	var a = jQuery('<a target="_blank" >My current Location</a>');
 
-	li.text(`${message.from}: `);
+	li.text(`${message.from} ${formattedTime}: `);
 	a.attr('href', message.url);
 	li.append(a);
 	jQuery('#messages').append(li);
