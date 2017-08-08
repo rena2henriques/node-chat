525adc23e3619812777dd1c69f07cc9781d89267
rena2henriques <renato.manao@gmail.com>
2017-08-08 16:11:49 +0100

    autoscroll to bottom if user is close to end of page

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,26 @@
 // to begin using socket.io
 var socket = io();
 
+// function to determine if there is a need to scroll the page or not
+function scrollToBottom () {
+	// Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	} 
+}
+
 // used to detect when a user connects
 socket.on('connect', function() {
 	console.log('Connected to server.');
-
 });
 
 // used to detect when a user disconnects
@@ -25,7 +41,7 @@ socket.on('newMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
-	
+	scrollToBottom();	
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -39,7 +55,7 @@ socket.on('newLocationMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
-
+	scrollToBottom();	
 });
 
 jQuery('#message-form').on('submit', function(e) {
